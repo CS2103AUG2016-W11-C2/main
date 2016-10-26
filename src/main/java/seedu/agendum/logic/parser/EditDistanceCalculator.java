2aa856eab674faa169aea7ecd5d45fca7fb7c07c
Vishnu Prem <vishnu@vishnuprem.com>
2016-10-26 11:32:35 +0800

    Add comments about online code

--- src/main/java/seedu/agendum/logic/parser/EditDistanceCalculator.java
+++ src/main/java/seedu/agendum/logic/parser/EditDistanceCalculator.java
@@ -46,15 +46,14 @@ public class EditDistanceCalculator {
         }
     }
 
+    // Code from https://rosettacode.org/wiki/Levenshtein_distance#Java
     public static int distance(String a, String b) {
         a = a.toLowerCase();
         b = b.toLowerCase();
-        // i == 0
         int [] costs = new int [b.length() + 1];
         for (int j = 0; j < costs.length; j++)
             costs[j] = j;
         for (int i = 1; i <= a.length(); i++) {
-            // j == 0; nw = lev(i - 1, j)
             costs[0] = i;
             int nw = i - 1;
             for (int j = 1; j <= b.length(); j++) {
