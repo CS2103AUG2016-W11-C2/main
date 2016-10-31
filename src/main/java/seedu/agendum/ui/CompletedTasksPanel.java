a199551e3ef432e3731784a229b8da2eb1aa3d58
Fan Weiguang <lahalaha90@gmail.com>
2016-10-31 19:32:58 +0800

    Disable click on the list

--- src/main/java/seedu/agendum/ui/CompletedTasksPanel.java
+++ src/main/java/seedu/agendum/ui/CompletedTasksPanel.java
@@ -3,12 +3,14 @@ package seedu.agendum.ui;
 import javafx.animation.PauseTransition;
 import javafx.application.Platform;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Control;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MultipleSelectionModel;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.input.MouseEvent;
 import javafx.util.Duration;
 import seedu.agendum.model.task.ReadOnlyTask;
 import seedu.agendum.model.task.Task;
@@ -35,22 +37,36 @@ public class CompletedTasksPanel extends TasksPanel {
         mainTaskList = taskList;
         completedTasksListView.setItems(taskList.filtered(task -> task.isCompleted()));
         completedTasksListView.setCellFactory(listView -> new CompletedTasksListViewCell());
+        configure();
+    }
+    
+    private void configure() {
         selectionModel = completedTasksListView.getSelectionModel();
         completedTasksListView.setSelectionModel(null);
+        completedTasksListView.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                event.consume();
+            }
+        });
     }
 
     public void scrollTo(Task task, boolean hasMultipleTasks) {
         Platform.runLater(() -> {
+            
             int index = mainTaskList.indexOf(task) - mainTaskList.filtered(t -> !t.isCompleted()).size();
             completedTasksListView.scrollTo(index);
             completedTasksListView.setSelectionModel(selectionModel);
+            
             if(hasMultipleTasks) {
                 completedTasksListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
                 completedTasksListView.getSelectionModel().select(index);
             } else {
                 completedTasksListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
                 completedTasksListView.getSelectionModel().clearAndSelect(index);
+                completedTasksListView.setSelectionModel(null);
             }
+            
             PauseTransition delay = new PauseTransition(Duration.seconds(5));
             delay.setOnFinished(event -> completedTasksListView.getSelectionModel().clearSelection(index));
             delay.play();
--- src/main/java/seedu/agendum/ui/FloatingTasksPanel.java
+++ src/main/java/seedu/agendum/ui/FloatingTasksPanel.java
@@ -3,12 +3,14 @@ package seedu.agendum.ui;
 import javafx.animation.PauseTransition;
 import javafx.application.Platform;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Control;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.MultipleSelectionModel;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.input.MouseEvent;
 import javafx.util.Duration;
 import seedu.agendum.model.task.ReadOnlyTask;
 import seedu.agendum.model.task.Task;
@@ -35,23 +37,37 @@ public class FloatingTasksPanel extends TasksPanel {
         mainTaskList = taskList;
         floatingTasksListView.setItems(taskList.filtered(task -> !task.isCompleted() && !task.hasTime()));
         floatingTasksListView.setCellFactory(listView -> new FloatingTasksListViewCell());
+        configure();
+    }
+    
+    private void configure() {
         selectionModel = floatingTasksListView.getSelectionModel();
         floatingTasksListView.setSelectionModel(null);
+        floatingTasksListView.addEventFilter(MouseEvent.MOUSE_PRESSED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                event.consume();
+            }
+        });
     }
 
     public void scrollTo(Task task, boolean hasMultipleTasks) {
         Platform.runLater(() -> {
+            
             int index = mainTaskList.indexOf(task) - 
                     mainTaskList.filtered(t -> (t.hasTime() && !t.isCompleted())).size();
             floatingTasksListView.scrollTo(index);
             floatingTasksListView.setSelectionModel(selectionModel);
+            
             if(hasMultipleTasks) {
                 floatingTasksListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
                 floatingTasksListView.getSelectionModel().select(index);
             } else {
                 floatingTasksListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
                 floatingTasksListView.getSelectionModel().clearAndSelect(index);
+                floatingTasksListView.setSelectionModel(null);
             }
+            
             PauseTransition delay = new PauseTransition(Duration.seconds(4));
             delay.setOnFinished(event -> floatingTasksListView.getSelectionModel().clearSelection(index));
             delay.play();
