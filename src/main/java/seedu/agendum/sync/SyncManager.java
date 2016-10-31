59629af7c1377a00cb00a6dc97def1afb4349a5d
Vishnu Prem <vishnu@vishnuprem.com>
2016-10-31 20:04:55 +0800

    SyncManager

--- src/main/java/seedu/agendum/sync/SyncManager.java
+++ src/main/java/seedu/agendum/sync/SyncManager.java
@@ -1,5 +1,44 @@
 package seedu.agendum.sync;
 
-public class SyncManager {
-    
+import seedu.agendum.commons.core.ComponentManager;
+import seedu.agendum.commons.core.LogsCenter;
+import seedu.agendum.model.task.Task;
+
+import java.util.logging.Logger;
+
+public class SyncManager extends ComponentManager implements Sync {
+    private final Logger logger = LogsCenter.getLogger(SyncManager.class);
+    private SyncStatus syncStatus = SyncStatus.NOTRUNNING;
+
+    private final SyncProvider syncProvider;
+
+    public SyncManager(SyncProvider syncProvider) {
+        this.syncProvider = syncProvider;
+        this.syncProvider.setManager(this);
+    }
+
+    @Override
+    public SyncStatus getSyncStatus() {
+        return syncStatus;
+    }
+
+    @Override
+    public void setSyncStatus(SyncStatus syncStatus) {
+        this.syncStatus = syncStatus;
+    }
+
+    @Override
+    public void startSyncing() {
+        syncProvider.initialize();
+    }
+
+    @Override
+    public void stopSyncing() {
+
+    }
+
+    @Override
+    public void addNewEvent(Task task) {
+
+    }
 }
