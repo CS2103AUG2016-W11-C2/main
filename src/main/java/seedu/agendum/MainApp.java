71ccb8db7127a09d021ccd9b04218c57e619e68d
rachx <sim.rachael@hotmail.com>
2016-10-31 23:05:48 +0800

    remove unused variable

--- src/main/java/seedu/agendum/MainApp.java
+++ src/main/java/seedu/agendum/MainApp.java
@@ -41,7 +41,6 @@ public class MainApp extends Application {
     protected Model model;
     protected Config config;
     protected UserPrefs userPrefs;
-    protected CommandLibrary commandLibrary;
 
     public MainApp() {}
 
