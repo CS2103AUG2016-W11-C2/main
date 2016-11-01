f7b03a23099f94302fa67961cdbf45deb84a7070
rachx <sim.rachael@hotmail.com>
2016-11-01 14:36:17 +0800

    solution?

--- src/main/java/seedu/agendum/model/task/Task.java
+++ src/main/java/seedu/agendum/model/task/Task.java
@@ -205,8 +205,8 @@ public class Task implements ReadOnlyTask, Comparable<Task> {
 
     public int compareLastUpdatedTime(Task other) {
         // to fix erratic behavior for logic manager test
-        long seconds = ChronoUnit.SECONDS.between(this.getLastUpdatedTime(), other.getLastUpdatedTime());
-        if (Math.abs(seconds) < 2) {
+        long nanoseconds = ChronoUnit.NANOS.between(this.getLastUpdatedTime(), other.getLastUpdatedTime());
+        if (Math.abs(nanoseconds) < 10) {
             return 0;
         }
         return other.getLastUpdatedTime().compareTo(this.getLastUpdatedTime());
