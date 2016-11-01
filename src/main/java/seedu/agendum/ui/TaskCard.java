efec90093b6caedf76280b0ded97610ff38f0ad2
rachx <sim.rachael@hotmail.com>
2016-11-01 22:54:22 +0800

    Format time

--- src/main/java/seedu/agendum/ui/TaskCard.java
+++ src/main/java/seedu/agendum/ui/TaskCard.java
@@ -18,19 +18,19 @@ import seedu.agendum.model.task.ReadOnlyTask;
 
 //@@author A0148031R
 public class TaskCard extends UiPart {
-    
+
     private static final String FXML = "TaskCard.fxml";
     private static final String OVERDUE_PREFIX = "Overdue\n";
-    private static final String COMPLETED_PREFIX = "Completed ";
+    private static final String COMPLETED_PREFIX = "Completed on ";
     private static final String NAME_COLOR = "#3a3d42";
     private static final String TIME_COLOR = "#4172c1";
-    private static final String NON_COMPLETED_TIME_PATTERN = "HH:mm EEE, dd MMM";
+    private static final String TASK_TIME_PATTERN = "HH:mm EEE, dd MMM";
     private static final String COMPLETED_TIME_PATTERN = "EEE, dd MMM";
     private static final String START_TIME_PREFIX = "from ";
     private static final String END_TIME_PREFIX = " to ";
     private static final String DEADLINE_PREFIX = "by ";
     private static final String EMPTY_PREFIX = "";
-    
+
     @FXML
     private HBox cardPane;
     @FXML
@@ -43,9 +43,10 @@ public class TaskCard extends UiPart {
     private ReadOnlyTask task;
     private String displayedIndex;
 
-    public TaskCard() {}
+    public TaskCard() {
+    }
 
-    public static TaskCard load(ReadOnlyTask task, int Index){
+    public static TaskCard load(ReadOnlyTask task, int Index) {
         TaskCard card = new TaskCard();
         card.task = task;
         card.displayedIndex = String.valueOf(Index) + ".";
@@ -54,61 +55,80 @@ public class TaskCard extends UiPart {
 
     @FXML
     public void initialize() {
-        
-        if(task.isOverdue()) {
+
+        if (task.isOverdue()) {
             cardPane.setStyle("-fx-background-color: rgb(255, 169, 147)");
-        } else if(task.isUpcoming()) {
+        } else if (task.isUpcoming()) {
             cardPane.setStyle("-fx-background-color: rgb(255, 229, 86)");
         } else {
             cardPane.setStyle("-fx-background-color: rgba(255,255,255,0.6)");
         }
-        
+
         name.setTextFill(Color.web(NAME_COLOR));
         name.setText(task.getName().fullName);
         id.setText(displayedIndex);
-        
+
         Label time = new Label();
         time.setMaxHeight(Control.USE_COMPUTED_SIZE);
         time.setTextFill(Color.web(TIME_COLOR));
         time.setWrapText(true);
-        
-        if(task.isOverdue()) {
-            time.setText(OVERDUE_PREFIX 
-                    + formatTime(NON_COMPLETED_TIME_PATTERN, START_TIME_PREFIX, task.getStartDateTime()) 
-                    + formatTime(NON_COMPLETED_TIME_PATTERN, END_TIME_PREFIX, task.getEndDateTime()));
-        } else if(task.hasTime()){
-            time.setText(formatTime(NON_COMPLETED_TIME_PATTERN, START_TIME_PREFIX, task.getStartDateTime()) 
-                    + formatTime(NON_COMPLETED_TIME_PATTERN, END_TIME_PREFIX, task.getEndDateTime()));
-        } else if(task.isCompleted()) {
-            time.setText(COMPLETED_PREFIX 
-                    + formatTime(COMPLETED_TIME_PATTERN, EMPTY_PREFIX, 
-                            Optional.ofNullable(task.getLastUpdatedTime())));
+
+        StringBuilder timeDescription = new StringBuilder();
+        timeDescription.append(formatTaskTime(task));
+
+        if (task.isCompleted()) {
+            timeDescription.append(formatUpdatedTime(task));
         }
-        
-        if(task.hasTime() || task.isCompleted()) {
+
+        time.setText(timeDescription.toString());
+
+        if (task.hasTime() || task.isCompleted()) {
             taskVbox.getChildren().add(time);
             taskVbox.setAlignment(Pos.CENTER_LEFT);
             time.setAlignment(Pos.CENTER_LEFT);
             time.setFont(Font.font("Verdana", FontPosture.ITALIC, 11));
         }
     }
-    
+
     public String formatTime(String dateTimePattern, String prefix, Optional<LocalDateTime> dateTime) {
-        
+
         StringBuilder sb = new StringBuilder();
         DateTimeFormatter format = DateTimeFormatter.ofPattern(dateTimePattern);
+        sb.append(prefix).append(dateTime.get().format(format));
         
-        if(task.isCompleted()) {
-            sb.append(dateTime.get().format(format));
-        } else if (dateTime.isPresent() && task.getStartDateTime().isPresent()) {
-            sb.append(prefix).append(dateTime.get().format(format));
-        } else if(dateTime.isPresent()) {
-            sb.append(DEADLINE_PREFIX).append(dateTime.get().format(format));
-        } else {
-            sb.append(EMPTY_PREFIX);
+        return sb.toString();
+    }
+
+    public String formatTaskTime(ReadOnlyTask task) {
+        
+        StringBuilder timeStringBuilder = new StringBuilder();
+
+        if (task.isOverdue()) {
+            timeStringBuilder.append(OVERDUE_PREFIX);
         }
-        
-        return sb.toString().toLowerCase();
+
+        if (task.isEvent()) {
+            String startTime = formatTime(TASK_TIME_PATTERN, START_TIME_PREFIX, task.getStartDateTime());
+            String endTime = formatTime(TASK_TIME_PATTERN, END_TIME_PREFIX, task.getEndDateTime());
+            timeStringBuilder.append(startTime);
+            timeStringBuilder.append(endTime);
+        } else if (task.hasDeadline()) {
+            String deadline = formatTime(TASK_TIME_PATTERN, DEADLINE_PREFIX, task.getEndDateTime());
+            timeStringBuilder.append(deadline);
+        }
+
+        return timeStringBuilder.toString();
+    }
+
+    public String formatUpdatedTime(ReadOnlyTask task) {
+        StringBuilder timeStringBuilder = new StringBuilder();
+        if (task.hasTime()) {
+            timeStringBuilder.append("\n");
+        }
+        timeStringBuilder.append(COMPLETED_PREFIX);
+        timeStringBuilder.append(formatTime(COMPLETED_TIME_PATTERN, EMPTY_PREFIX,
+                Optional.ofNullable(task.getLastUpdatedTime())));
+        return timeStringBuilder.toString();
     }
 
     public HBox getLayout() {
@@ -117,7 +137,7 @@ public class TaskCard extends UiPart {
 
     @Override
     public void setNode(Node node) {
-        cardPane = (HBox)node;
+        cardPane = (HBox) node;
     }
 
     @Override
