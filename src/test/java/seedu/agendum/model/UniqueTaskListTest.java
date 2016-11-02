c7cecbeb5ebd474f334a87cf76c15f2a79c63fc9
rachx <sim.rachael@hotmail.com>
2016-11-02 19:37:41 +0800

    add unique task list test

--- src/test/java/seedu/agendum/model/UniqueTaskListTest.java
+++ src/test/java/seedu/agendum/model/UniqueTaskListTest.java
@@ -7,6 +7,8 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import seedu.agendum.commons.exceptions.IllegalValueException;
 import seedu.agendum.model.task.Name;
 import seedu.agendum.model.task.Task;
@@ -14,24 +16,25 @@ import seedu.agendum.model.task.UniqueTaskList;
 import seedu.agendum.model.task.UniqueTaskList.DuplicateTaskException;
 import seedu.agendum.model.task.UniqueTaskList.TaskNotFoundException;
 
+//@@author A0133367E
 public class UniqueTaskListTest {
 
     private UniqueTaskList uniqueTaskList;
+    private ObservableList<Task> internalList;
+
     private Task originalTask;
     private Task duplicateOfOriginalTask;
     private Task newTask;
 
     @Before
-    public void setup() {
-        try {
-            uniqueTaskList = new UniqueTaskList();
-            originalTask = new Task(new Name("task"));
-            duplicateOfOriginalTask = new Task(originalTask);
-            newTask = new Task(new Name("new task"));
-            uniqueTaskList.add(originalTask);
-        } catch (IllegalValueException e) {
-            assert false : "valid name is given";
-        }
+    public void setup() throws IllegalValueException {
+        uniqueTaskList = new UniqueTaskList();
+        originalTask = new Task(new Name("task"));
+        duplicateOfOriginalTask = new Task(originalTask);
+        newTask = new Task(new Name("new task"));
+        uniqueTaskList.add(originalTask);
+        internalList = FXCollections.observableArrayList();
+        internalList.add(originalTask);
     }
 
     @Test
@@ -58,6 +61,8 @@ public class UniqueTaskListTest {
     @Test
     public void add_newTask_successful() throws Exception {
         uniqueTaskList.add(newTask);
+        internalList.add(newTask);
+        assertEquals(uniqueTaskList.getInternalList(), internalList);
     }
 
     @Test(expected = TaskNotFoundException.class)
@@ -68,6 +73,8 @@ public class UniqueTaskListTest {
     @Test
     public void remove_existingTask_successful() throws Exception {
         uniqueTaskList.remove(duplicateOfOriginalTask);
+        internalList.clear();
+        assertEquals(uniqueTaskList.getInternalList(), internalList);
     }
 
     @Test(expected = DuplicateTaskException.class)
@@ -84,6 +91,8 @@ public class UniqueTaskListTest {
     @Test
     public void update_existingTask_successful() throws Exception {
         uniqueTaskList.update(originalTask, newTask);
+        internalList.set(0, newTask);
+        assertEquals(uniqueTaskList.getInternalList(), internalList);
     }
 
     @Test(expected = DuplicateTaskException.class)
@@ -101,6 +110,8 @@ public class UniqueTaskListTest {
     @Test
     public void mark_existingTask_successful() throws Exception {
         uniqueTaskList.mark(originalTask);
+        internalList.get(0).markAsCompleted();
+        assertEquals(uniqueTaskList.getInternalList(), internalList);
     }
 
     @Test(expected = DuplicateTaskException.class)
@@ -120,5 +131,6 @@ public class UniqueTaskListTest {
         duplicateOfOriginalTask.markAsCompleted();
         uniqueTaskList.add(duplicateOfOriginalTask);
         uniqueTaskList.unmark(duplicateOfOriginalTask);
+        assertEquals(uniqueTaskList.getInternalList(), internalList);
     }
 }
