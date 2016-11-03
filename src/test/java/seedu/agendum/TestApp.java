afcf31ffee100820a263d5012e6da91c78ed8404
Justin <justintay123@gmail.com>
2016-11-03 17:35:44 +0800

    Change testapp config file path

--- src/test/java/seedu/agendum/TestApp.java
+++ src/test/java/seedu/agendum/TestApp.java
@@ -19,6 +19,7 @@ public class TestApp extends MainApp {
 
     public static final String SAVE_LOCATION_FOR_TESTING = TestUtil.getFilePathInSandboxFolder("sampleData.xml");
     protected static final String DEFAULT_PREF_FILE_LOCATION_FOR_TESTING = TestUtil.getFilePathInSandboxFolder("pref_testing.json");
+    protected static final String DEFAULT_CONFIG_FILE_LOCATION_FOR_TESTING = TestUtil.getFilePathInSandboxFolder("config_testing.json");
     public static final String APP_TITLE = "Test App";
     protected static final String TO_DO_LIST_NAME = "Test";
     protected Supplier<ReadOnlyToDoList> initialDataSupplier = () -> null;
@@ -42,7 +43,7 @@ public class TestApp extends MainApp {
 
     @Override
     protected Config initConfig(String configFilePath) {
-        Config config = super.initConfig(configFilePath);
+        Config config = super.initConfig(DEFAULT_CONFIG_FILE_LOCATION_FOR_TESTING);
         config.setAppTitle(APP_TITLE);
         config.setToDoListFilePath(saveFileLocation);
         config.setUserPrefsFilePath(DEFAULT_PREF_FILE_LOCATION_FOR_TESTING);
