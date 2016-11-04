5e8cba720cf4005ec33d9c3f4cf72ba0c4ad84d5
Justin <justintay123@gmail.com>
2016-11-04 20:59:57 +0800

    Move UserPrefsTest to model package

--- src/test/java/seedu/agendum/model/UserPrefsTest.java
+++ src/test/java/seedu/agendum/model/UserPrefsTest.java
@@ -1,4 +1,4 @@
-package seedu.agendum.commons.core;
+package seedu.agendum.model;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -7,36 +7,36 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
-import seedu.agendum.model.UserPrefs;
+import seedu.agendum.commons.core.GuiSettings;
 
 //@@author A0148095X
 public class UserPrefsTest {
 
     private UserPrefs one, another;
-    
+
     @Before
     public void setUp() {
         one = new UserPrefs();
         another = new UserPrefs();
     }
-    
+
     @Test
     public void equals_differentObject_returnsFalse() {
         assertFalse(one.equals(new Object()));
     }
-    
+
     @Test
     public void equals_symmetric_returnsTrue() {
         // equals to itself and object with same parameters
         assertTrue(one.equals(one));
-        assertTrue(one.equals(another));        
+        assertTrue(one.equals(another));
     }
-    
+
     @Test
     public void hashcode_symmetric_returnsTrue() {
         assertEquals(one.hashCode(), another.hashCode());
     }
-    
+
     @Test
     public void setGuiSettings_validInputs_successful() {
         final double expectedWidth = 222;
@@ -44,12 +44,12 @@ public class UserPrefsTest {
         final int expectedX = 444;
         final int expectedY = 555;
         final GuiSettings expectedGuiSettings = new GuiSettings(expectedWidth, expectedHeight, expectedX, expectedY);
-        
+
         final UserPrefs userPrefs = new UserPrefs();
         userPrefs.setGuiSettings(expectedWidth, expectedHeight, expectedX, expectedY);
         GuiSettings actualGuiSettings = userPrefs.getGuiSettings();
-        
+
         assertEquals(actualGuiSettings, expectedGuiSettings);
     }
-    
+
 }
