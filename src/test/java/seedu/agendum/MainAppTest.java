b1f7d7b43f09ad4a224616d0501661eeb3aacbc9
Justin <justintay123@gmail.com>
2016-11-04 20:24:51 +0800

    Add tests for initAliasTable

--- src/test/java/seedu/agendum/MainAppTest.java
+++ src/test/java/seedu/agendum/MainAppTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Hashtable;
 import java.util.Optional;
 
 import org.junit.Assert;
@@ -11,8 +12,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 import seedu.agendum.commons.core.Config;
+import seedu.agendum.commons.exceptions.DataConversionException;
 import seedu.agendum.commons.util.ConfigUtil;
+import seedu.agendum.logic.commands.CommandLibrary;
 import seedu.agendum.model.UserPrefs;
+import seedu.agendum.storage.JsonAliasTableStorage;
 import seedu.agendum.storage.JsonUserPrefsStorage;
 import seedu.agendum.storage.StorageManager;
 import seedu.agendum.testutil.TestUtil;
@@ -23,8 +27,13 @@ public class MainAppTest {
     private MainApp mainApp;
 
     private Config defaultConfig;
-
     private UserPrefs defaultUserPrefs;
+    private Hashtable<String, String> defaultAliasTable;
+    
+    // user prefs and alias table filepaths lead to empty files
+    private Config configWithBadFilePaths; 
+    // user prefs and alias table filepaths lead to read only files
+    private Config configWithReadOnlyFilePaths;
 
     private final String pathToBadConfig = TestUtil.getFilePathInSandboxFolder("bad_config.json");
     private final String pathToReadOnlyConfig = TestUtil.getFilePathInSandboxFolder("read_only_config.json");
@@ -32,19 +41,28 @@ public class MainAppTest {
     private final String pathToBadUserPrefs = TestUtil.getFilePathInSandboxFolder("bad_user_prefs.json");
     private final String pathToReadOnlyUserPrefs = TestUtil.getFilePathInSandboxFolder("read_only_user_prefs.json");
 
+    private final String pathToBadAliasTable = TestUtil.getFilePathInSandboxFolder("bad_alias_table.json");
+    private final String pathToReadOnlyAliasTable = TestUtil.getFilePathInSandboxFolder("read_only_alias_table.json");
+
     @Before
     public void setUp() {
         mainApp = new MainApp();
 
         defaultConfig = new Config();
-
         defaultUserPrefs = new UserPrefs();
+        defaultAliasTable = new Hashtable<String, String>();
+
+        configWithBadFilePaths = generateConfigWithBadFilePaths();
+        configWithReadOnlyFilePaths = generateConfigWithReadOnlyFilePaths();
 
         createEmptyFile(pathToBadConfig);
-        createReadOnlyConfigFile();
+        createReadOnlyConfigFile(pathToReadOnlyConfig);
 
         createEmptyFile(pathToBadUserPrefs);
-        createReadOnlyUserPrefsFile();
+        createReadOnlyUserPrefsFile(pathToReadOnlyUserPrefs);
+
+        createEmptyFile(pathToBadAliasTable);
+        createReadOnlyAliasTableFile(pathToReadOnlyAliasTable);
     }
 
     @Test
@@ -54,60 +72,97 @@ public class MainAppTest {
     }
 
     @Test
-    public void initConfig_validFilePathButInvalidFileFormat_returnsDefaultConfig() {
+    public void initConfig_validFilePathInvalidFileFormat_returnsDefaultConfig() {
         Config config = mainApp.initConfig(pathToBadConfig);
         assertEquals(config, defaultConfig);
     }
 
     @Test
-    public void initConfig_validFilePathValidFormatButUnwriteable_returnsDefaultConfigLogsWarning() {
+    public void initConfig_validFilePathValidFormatReadOnly_returnsDefaultConfigLogsWarning() {
         Config config = mainApp.initConfig(pathToReadOnlyConfig);
         assertEquals(config, defaultConfig);
     }
 
     @Test
-    public void initPrefs_invalidUserPrefsFileFormat_returnsDefaultUserPrefs() {
-        // Set up a config to point to bad user prefs
-        Config config = new Config();
-        config.setUserPrefsFilePath(pathToBadUserPrefs);
-        mainApp.storage = new CustomUserPrefsStorageManagerStub(pathToBadUserPrefs);
+    public void initPrefs_invalidFileFormat_returnsDefaultUserPrefs() {
+        // Set up storage to point to bad user prefs
+        mainApp.storage = new StorageManager("", "", pathToBadUserPrefs, null);
 
-        UserPrefs userPrefs = mainApp.initPrefs(config);
+        UserPrefs userPrefs = mainApp.initPrefs(configWithBadFilePaths);
         assertEquals(userPrefs, defaultUserPrefs);
 
+        // reset storage
         mainApp.storage = null;
     }
 
     @Test
-    public void initPrefs_validUserPrefsFileFormatButUnwriteable_returnsDefaultUserPrefsLogsWarning() {
-        // Set up a config to point to read only prefs
-        Config config = new Config();
-        config.setUserPrefsFilePath(pathToReadOnlyUserPrefs);
-        mainApp.storage = new CustomUserPrefsStorageManagerStub(pathToReadOnlyUserPrefs);
+    public void initPrefs_validFileReadOnly_returnsDefaultUserPrefsLogsWarning() {
+        // Set up storage to point to read only prefs
+        mainApp.storage = new StorageManager("", "", pathToReadOnlyUserPrefs, null);
 
-        UserPrefs userPrefs = mainApp.initPrefs(config);
+        UserPrefs userPrefs = mainApp.initPrefs(configWithReadOnlyFilePaths);
         assertEquals(userPrefs, defaultUserPrefs);
 
+        // reset storage
         mainApp.storage = null;
     }
 
     @Test
     public void initPrefs_exceptionThrowingStorage_returnsDefaultUserPrefsLogsWarning() {
-        mainApp.storage = new ExceptionThrowingStorageManagerStub();
+        mainApp.storage = new ReadUserPrefsExceptionThrowingStorageManagerStub();
 
         UserPrefs userPrefs = mainApp.initPrefs(defaultConfig);
         assertEquals(userPrefs, defaultUserPrefs);
 
+        // reset storage
         mainApp.storage = null;
     }
 
+    @Test
+    public void initAliasTable_invalidFileFormat_returnsEmptyHashtable() {
+        // Set up storage to point to bad alias table
+        mainApp.storage = new StorageManager("", pathToBadAliasTable, "", null);
+
+        mainApp.initAliasTable(configWithBadFilePaths);
+        Hashtable<String, String> actualAliasTable = CommandLibrary.getInstance().getAliasTable();
+        assertEquals(actualAliasTable, defaultAliasTable);
+
+        // reset storage and alias table
+        mainApp.storage = null;
+        CommandLibrary.getInstance().loadAliasTable(null);
+    }
+
+    @Test
+    public void initAliasTable_validFileFormatReadOnly_returnsEmptyHashtable() {
+        // Set up storage to point to read only alias table
+        mainApp.storage = new StorageManager("", pathToReadOnlyAliasTable, "", null);
+
+        mainApp.initAliasTable(configWithReadOnlyFilePaths);
+        Hashtable<String, String> actualAliasTable = CommandLibrary.getInstance().getAliasTable();
+        assertEquals(actualAliasTable, defaultAliasTable);
+
+        // reset storage and alias table
+        mainApp.storage = null;
+        CommandLibrary.getInstance().loadAliasTable(null);
+    }
+
+    @Test
+    public void initAliasTable_exceptionThrowingStorage_returnsDefaultHashtableLogsWarning() {
+        mainApp.storage = new ReadAliasTableExceptionThrowingStorageManagerStub();
+
+        mainApp.initAliasTable(defaultConfig);
+        Hashtable<String, String> actualAliasTable = CommandLibrary.getInstance().getAliasTable();
+        assertEquals(actualAliasTable, defaultAliasTable);
+
+        // reset storage and alias table
+        mainApp.storage = null;
+        CommandLibrary.getInstance().loadAliasTable(null);
+    }
+    
     private void createEmptyFile(String filePath) {
         File file = new File(filePath);
 
-        // Ensure that the file is empty
-        if (file.exists()) {
-            file.delete();
-        }
+        deleteIfExists(file);
 
         try {
             file.createNewFile();
@@ -116,16 +171,14 @@ public class MainAppTest {
         }
     }
 
-    private void createReadOnlyConfigFile() {
-        File file = new File(pathToReadOnlyConfig);
+    private void createReadOnlyConfigFile(String filePath) {
+        File file = new File(filePath);
 
         // Ensure that the file is empty
-        if (file.exists()) {
-            file.delete();
-        }
+        deleteIfExists(file);
 
         try {
-            ConfigUtil.saveConfig(defaultConfig, pathToReadOnlyConfig);
+            ConfigUtil.saveConfig(defaultConfig, filePath);
         } catch (IOException e) {
             Assert.fail("Error creating read only config file");
         }
@@ -135,17 +188,15 @@ public class MainAppTest {
         }
     }
 
-    private void createReadOnlyUserPrefsFile() {
-        File file = new File(pathToReadOnlyUserPrefs);
+    private void createReadOnlyUserPrefsFile(String filePath) {
+        File file = new File(filePath);
 
         // Ensure that the file is empty
-        if (file.exists()) {
-            file.delete();
-        }
+        deleteIfExists(file);
 
         try {
-            JsonUserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(pathToReadOnlyUserPrefs);
-            userPrefsStorage.saveUserPrefs(defaultUserPrefs, pathToReadOnlyUserPrefs);
+            JsonUserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(filePath);
+            userPrefsStorage.saveUserPrefs(defaultUserPrefs, filePath);
         } catch (IOException e) {
             Assert.fail("Error creating read only user prefs file");
         }
@@ -155,20 +206,64 @@ public class MainAppTest {
         }
     }
 
-    class CustomUserPrefsStorageManagerStub extends StorageManager {
-        public CustomUserPrefsStorageManagerStub(String userPrefsPath) {
-            super("", "", userPrefsPath, null);
+    private void createReadOnlyAliasTableFile(String filePath) {
+        File file = new File(filePath);
+
+        // Ensure that the file is empty
+        deleteIfExists(file);
+
+        try {
+            JsonAliasTableStorage aliasTableStorage = new JsonAliasTableStorage(filePath);
+            aliasTableStorage.saveAliasTable(defaultAliasTable, filePath);
+        } catch (IOException e) {
+            Assert.fail("Error creating read only alias table file");
+        }
+
+        if (!file.setReadOnly()) {
+            Assert.fail("Unable to set read only alias table to read only");
         }
     }
 
-    class ExceptionThrowingStorageManagerStub extends StorageManager {
-        public ExceptionThrowingStorageManagerStub() {
+    public void deleteIfExists(File file) {
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
+    private Config generateConfigWithReadOnlyFilePaths() {
+        Config config = new Config();
+        config.setAliasTableFilePath(pathToReadOnlyAliasTable);
+        config.setUserPrefsFilePath(pathToReadOnlyUserPrefs);
+        return config;
+    }
+
+    private Config generateConfigWithBadFilePaths() {
+        Config config = new Config();
+        config.setAliasTableFilePath(pathToBadAliasTable);
+        config.setUserPrefsFilePath(pathToBadUserPrefs);
+        return config;        
+    }
+
+    /** Throws an IOException when readUserPrefs is called **/
+    class ReadUserPrefsExceptionThrowingStorageManagerStub extends StorageManager {
+        public ReadUserPrefsExceptionThrowingStorageManagerStub() {
+            super("", "", "", null);
+        }
+
+        public Optional<UserPrefs> readUserPrefs() throws IOException {
+            throw new IOException(this.getClass().getCanonicalName() +": IOException");
+        }
+    }    
+
+    /** Throws an IOException when readAliasTable is called **/
+    class ReadAliasTableExceptionThrowingStorageManagerStub extends StorageManager {
+        public ReadAliasTableExceptionThrowingStorageManagerStub() {
             super("", "", "", null);
         }
 
         @Override
-        public Optional<UserPrefs> readUserPrefs() throws IOException {
-            throw new IOException("ExceptionThrowingStorageManagerStub: IOException");
+        public Optional<Hashtable<String, String>> readAliasTable() throws DataConversionException, IOException {
+            throw new IOException(this.getClass().getCanonicalName() + ": IOException");
         }
     }
 }
