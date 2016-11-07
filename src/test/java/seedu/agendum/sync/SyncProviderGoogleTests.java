694a0573b057c22c375e83680470b6c58ed17123
Vishnu Prem <vishnu@vishnuprem.com>
2016-11-07 12:47:07 +0800

    Updat tests

--- src/test/java/seedu/agendum/sync/SyncProviderGoogleTests.java
+++ src/test/java/seedu/agendum/sync/SyncProviderGoogleTests.java
@@ -2,7 +2,8 @@ package seedu.agendum.sync;
 
 import org.junit.*;
 import org.junit.runners.MethodSorters;
-import org.mockito.internal.matchers.Any;
+import seedu.agendum.commons.exceptions.IllegalValueException;
+import seedu.agendum.model.task.Name;
 import seedu.agendum.model.task.Task;
 
 import java.io.File;
@@ -14,6 +15,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 
+import static junit.framework.TestCase.assertFalse;
 import static org.mockito.Mockito.*;
 import static seedu.agendum.commons.core.Config.DEFAULT_DATA_DIR;
 
@@ -30,10 +32,26 @@ public class SyncProviderGoogleTests {
 
     private static final SyncProviderGoogle syncProviderGoogle = spy(new SyncProviderGoogle());
     private static final SyncManager mockSyncManager = mock(SyncManager.class);
+    private static final Task mockTask = mock(Task.class);
 
     @BeforeClass
     public static void setUp() {
         copyTestCredentials();
+
+        try {
+            Optional<LocalDateTime> fakeTime = Optional.of(LocalDateTime.now());
+            Name fakeName = new Name("AGENDUMTESTENGINE");
+            int minId = 99999;
+            int maxId = 9999999;
+            Random r = new Random();
+
+            when(mockTask.getStartDateTime()).thenReturn(fakeTime);
+            when(mockTask.getEndDateTime()).thenReturn(fakeTime);
+            when(mockTask.getName()).thenReturn(fakeName);
+            when(mockTask.syncCode()).thenReturn(r.nextInt((maxId - minId) + 1) + minId);
+        } catch (IllegalValueException e) {
+            e.printStackTrace();
+        }
 
         syncProviderGoogle.setManager(mockSyncManager);
         syncProviderGoogle.start();
@@ -64,46 +82,50 @@ public class SyncProviderGoogleTests {
 
     @Test
     public void syncProviderGoogle_start_createCalendar() {
+        reset(syncProviderGoogle);
+        syncProviderGoogle.deleteAgendumCalendar();
+        syncProviderGoogle.start();
+
         // Verify if Sync Manager's status was changed
         verify(mockSyncManager, atLeastOnce()).setSyncStatus(Sync.SyncStatus.RUNNING);
     }
 
     @Test
     public void syncProviderGoogle_startIfNeeded_credentialsFound() {
+        reset(syncProviderGoogle);
         syncProviderGoogle.startIfNeeded();
 
         // Verify Sync Provider did start
-        verify(syncProviderGoogle, atLeast(2)).start();
+        verify(syncProviderGoogle).start();
     }
 
     @Test
     public void syncProviderGoogle_startIfNeeded_credentialsNotFound() {
+        reset(syncProviderGoogle);
         deleteCredential();
         syncProviderGoogle.startIfNeeded();
 
         // Verify Sync Provider should not start
-        verify(syncProviderGoogle, atLeastOnce()).start();
+        verify(syncProviderGoogle, never()).start();
+    }
+
+    @Test
+    public void syncProviderGoogle_stop_successful() {
+        reset(mockSyncManager);
+        syncProviderGoogle.stop();
+
+        // Verify sync status changed
+        verify(mockSyncManager).setSyncStatus(Sync.SyncStatus.NOTRUNNING);
+        assertFalse(DATA_STORE_CREDENTIAL.exists());
     }
 
     @Test
     public void syncProviderGoogle_addEvent_successful() {
-        Task mockTask = mock(Task.class);
-        Optional<LocalDateTime> fakeTime = Optional.of(LocalDateTime.now());
-
-        when(mockTask.getStartDateTime()).thenReturn(fakeTime);
-        when(mockTask.getEndDateTime()).thenReturn(fakeTime);
-
         syncProviderGoogle.addNewEvent(mockTask);
     }
 
     @Test
     public void syncProviderGoogle_deleteEvent_successful() {
-        Task mockTask = mock(Task.class);
-        Optional<LocalDateTime> fakeTime = Optional.of(LocalDateTime.now());
-
-        when(mockTask.getStartDateTime()).thenReturn(fakeTime);
-        when(mockTask.getEndDateTime()).thenReturn(fakeTime);
-
         syncProviderGoogle.deleteEvent(mockTask);
     }
 
